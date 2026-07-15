6ac1484cf4a20505abbef5f0e265828d34f45924
m-faizin <mohfaizin17@gmail.com>
2026-07-16 05:35:03 +0700

    plugin nitro untuk vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,16 @@
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { nitro } from 'nitro/vite'
+import viteReact from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
-    server: { entry: "server" },
-  },
+  plugins: [
+    tsconfigPaths(),
+    tanstackStart(),
+    nitro(), // Tambahkan plugin nitro di sini
+    viteReact(),
+  ],
 });
