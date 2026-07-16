f0dc7640c5124547144715560b38804cda15cb7e
m-faizin <mohfaizin17@gmail.com>
2026-07-16 07:36:34 +0700

    Delete src/client.tsx

--- src/client.tsx
+++
@@ -1,7 +0,0 @@
-import { createStartHandler } from '@tanstack/react-start/client'
-import { createRouter } from './src/router'
-
-const router = createRouter()
-const handler = createStartHandler({ router })
-
-handler()
